72a9293883c5a26cea151e5dbe96d31d3ddc744a
zloeffle <zloeffle@iu.edu>
2020-06-20 23:11:33 -0500

    Regression,Decision Tree,KNN

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,66 +1,158 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import math
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report,confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
 
 '''
-Example using kyphosis dataset
-
-Nodes
-- split for the value of an attribute
-Leaves
-- terminal nodes that predict the outcome
-Entropy
-- H(S) = -sum[p_i(S) * log_2(p_i(S))]
-Info Gain
-- IG(S,A) = H(S) - sum v->Vals(A)[(S_v/S) * H(S_v)]
-Random Forests
-- to improve performance, we can use many trees with a random sample of features chosen as the split
-- a new rand sample of features is chosen for every tree at every split
-- m = sqrt(p)
+Representation of a node in the tree
 '''
+class Node:
+    def __init__(self,feature,thresh,gini):
+        self.feature = feature 
+        self.threshold = thresh
+        self.gini = gini # split cost
+        self.left = None # left child
+        self.right = None # right child
+        self.is_terminal = False
+
 class DecisionTree:
-    def __init__(self, data):
-        self.data = pd.read_csv(data)
-        
-    def details(self):
-        data = self.data
-        print(data.columns)
-        print(data.shape)
+    def __init__(self,data,max_depth=5):
+        self.max_depth = max_depth
+        self.data = data
+
+    def explore_data(self,data):
         print(data.head())
         print(data.info())
         print(data.describe())
-        print(data.isnull().sum()) 
 
-    def predict(self):
-        df = self.data
+    # prints a string representation of a tree
+    def print(self,node,depth=0):
+        if isinstance(node,Node):
+            print('%s[%s < %.3f] Gini = %.4f' % (depth*' ',node.feature, node.threshold, node.gini))
+            self.print(node.left, depth+1)
+            self.print(node.right, depth+1)
+        else:
+            print('%s[%s]' % (depth*' ',node))
+
+    # paritions data into left and right splits based on a feature and threshold
+    def partition(self,data,feature,threshold):
+        left,right = [],[]
+
+        for i in list(data.index):
+            if data.loc[i,feature] >= threshold:
+                left.append(i)
+            else:
+                right.append(i)
+        return left,right
+
+    # gini = 1-sum[P_i * P_i]
+    # calculates impurity of a data group
+    def gini_impurity(self,data,groups):
+        gini = 0.0
+        instances = sum([len(group) for group in groups])
+        target = list(data.columns)[-1]
         
-        x = df.drop('Kyphosis', axis=1)
-        y = df['Kyphosis']
-        x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.30)
+        for group in groups:
+            n = len(group)
+            if n == 0:
+                continue
+
+            zero,one = 0,0
+            for index in group:
+                if data.loc[index,target] == 0:
+                    zero += 1
+                else:
+                    one += 1
+            gini += (1- (math.pow(zero/n,2) + math.pow(one/n,2))) * (n/instances)
         
-        # decision tree
-        dt = DecisionTreeClassifier()
-        dt.fit(x_train, y_train)
-        dt_pred = dt.predict(x_test)
-        
-        # random forest
-        rf = RandomForestClassifier(100)
-        rf.fit(x_train, y_train)
-        rf_pred = rf.predict(x_test)
-        
-        print(dt_pred, rf_pred)
-        
-        print(confusion_matrix(y_test,dt_pred))
-        print(classification_report(y_test,dt_pred))
-        print(confusion_matrix(y_test,rf_pred))
-        print(classification_report(y_test,rf_pred))
-        return dt,rf
-        
+        gini = round(gini,5)
+        return gini
+
+    # finds the optimal split in the dataset
+    def best_split(self,data):
+        best_feature,best_threshold,best_gini,best_groups = None,math.inf,math.inf,None
+        features = list(data.columns[:-1])
+        indicies = list(data.index)
+        for col in features:
+            for row in indicies:
+                groups = self.partition(data,col,data.loc[row,col])
+                gini = self.gini_impurity(data,groups)
+                
+                if gini < best_gini:
+                    best_gini = gini
+                    best_feature = col
+                    best_groups = groups
+                    best_threshold = data.loc[row,col]
+
+        node = Node(best_feature,best_threshold,best_gini)
+        node.left = best_groups[0]
+        node.right = best_groups[1]
+        return node
+
+    # makes a node terminal
+    def to_terminal(self,group):
+        cols = list(self.data.columns)
+        zero,one = 0,0
+        data = self.data
+
+        for row in group:
+            if data.loc[row,cols[-1]] == 0:
+                zero += 1
+            else:
+                one += 1
+        if zero > one:
+            return 0
+        return 1
+
+    # generates left and right children for a node or makes it terminal
+    def split_node(self,node,depth,min_size=1):
+        left,right = node.left,node.right
+
+        # check no split
+        if left is None or right is None:
+            node.left,node.right = self.to_terminal(left+right)
+            return
+
+        # check for max depth
+        if depth >= self.max_depth:
+            node.left = self.to_terminal(left)
+            node.right = self.to_terminal(right)
+            return
+
+        # process left child
+        if len(left) <= min_size:
+            node.left = self.to_terminal(left)
+        else:
+            node.left = self.best_split(left)
+            self.split(node.left, depth+1,min_size)
+
+        # process right child
+        if len(right) <= min_size:
+            node.right = self.to_terminal(right)
+        else:
+            node.right = self.best_split(right)
+            self.split(node.right, depth+1,min_size)
+
+    # build the decision tree model
+    def build_tree(self,data):
+        root = self.best_split(data) # root node is optimal split
+        self.split_node(root,10)
+        return root
+
 if __name__ == '__main__':
-    dt = DecisionTree('data/kyphosis.csv')
-    dt.details()
-    dt.predict()
+    df = pd.read_csv('data/Social_Network_Ads.csv')
+    model = DecisionTree(df)
+    #model.explore_data(df)
+
+    # split data into training/testing sets
+    cols = list(df.columns)
+    x = df[cols[:-1]]
+    y = df[cols[-1]]
+    x_train,x_test = x.iloc[:300],x.iloc[300:]
+    y_train,y_test = y.iloc[:300],y.iloc[300:]
+    
+    #left,right = model.partition(df,'Age')
+    #model.gini_impurity(df,[left,right])
+    #model.best_split(df)
+    root = model.build_tree(df)
+    model.print(root)
--- LinearRegression.py
+++ LinearRegression.py
@@ -1,131 +1,77 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import random
 
 '''
-Goal: minimize the vertical dist between all the data points and our line
-Mean Absolute Error is the easiest to understand, because it's the average error.
-Mean Squared Error is more popular than MAE, because MSE "punishes" larger errors, which tends to be useful in the real world.
-Root Mean Squared Error is even more popular than MSE, because RMSE is interpretable in the "y" units.
+y = B0 + B1*X1 + ... + Bn*Xn
+B = ((x_Transpose.x)^-1).x_Transpose.y
 '''
-
-class LinearRegression:
-    def __init__(self,data):
-        self.columns = []
+class MultipleLinearRegression:
+    def __init__(self):
         self.coef = [] # beta values
-        
+    
     def explore_data(self,data):
+        print(data.head())
         print(data.info())
         print(data.describe())
-        print(list(data.columns))
 
-    '''
-    params: data = pandas dataframe, features = features to drop from dataset, target = target feature in dataset
-    returns: tuple(x,y) where x = dataframe of independent vars and y = dataframe of dependent vars
-    '''
-    def prepare_data(self,data,features,target):
-        # remove irrelevant features
-        data.drop(features,axis=1,inplace=True)
-        
-        # get target column
-        y = data[target]
-        
-        # get predictor vars
-        data.drop(target,axis=1,inplace=True)
-        x = data
-
-        # set global column list
-        self.columns = list(x.columns)
-        
-        return x,y
-    
-    '''
-    Splits data into training and testing components
-    params: x = dataframe of predictor vars, y = dataframe of target vars, size = test size
-    '''
+    # split data into training and testing sets of specified size
     def train_test_split(self,x,y,size):
-        split_point = int(len(x) * size)      
-        x_train,y_train = x.iloc[:split_point].to_numpy(),y.iloc[:split_point].to_numpy()
-        x_test,y_test = x.iloc[split_point:].to_numpy(),y.iloc[split_point:].to_numpy()
+        split = int(len(x) * size) # point to split data
+
+        # training and testing data for independent and dependent vars
+        x_train,y_train = x.iloc[:split],y.iloc[:split]
+        x_test,y_test = x.iloc[split:],y.iloc[split:]
         return x_train,y_train,x_test,y_test
 
-    def reshape_x(self,x):
-        x = x.reshape(-1,1)
-        return x
-    
+    # adds column of 1's to data matrix
     def ones(self,x):
         ones = np.ones(shape=x.shape[0]).reshape(-1,1)
-        ones = np.concatenate((ones,x),1)
-        return ones
+        return np.concatenate((ones,x),1)
 
-    '''
-    Generates coefficients using the equation: b = (x_T dot x)^-1 dot x_T dot y
-    params: x = training data for independent vars, y = training data for dependent vars
-    returns: Nothing, sets adds coefficients to global array
-    '''
+    # fit training data to model
     def fit(self,x,y):
-        if (len(x.shape)) == 1:
-            x = self.reshape_x(x)
-        self.coef = np.linalg.inv(x.transpose().dot(x)).dot(x.transpose()).dot(y)
-    
-    '''
-    Validate regression model and make new predictions
-    params: x_test = testing data for independent vars, y_test = testing data for dependent vars
-    returns: dataframe with same features as x_test as well as columns for predicted and actual values
-    '''
-    def predict(self,x_test,y_test):
-        predictions = []
+        # add col of ones to x matrix
+        x = self.ones(x)
+        # generate coeficients 
+        coef = np.linalg.inv(x.transpose().dot(x)).dot(x.transpose()).dot(y)
+        self.coef = coef
+
+    # predict on new data
+    def predict(self,x_test):
+        preds = [] # predictions
         b0 = self.coef[0]
         betas = self.coef[1:]
-        
+        x_test = self.ones(x_test)
+
         for row in x_test:
             pred = b0
             for xi,bi in zip(row,betas):
                 pred += xi*bi
-            predictions.append(round(pred,2))
+            preds.append(pred)
+        return preds
         
-        x_test = pd.DataFrame(x_test,columns=self.columns)
-        x_test['Actual'] = y_test
-        x_test['Predicted'] = predictions
-        return x_test
     
-if __name__ == '__main__':    
-    '''
-    QUESTION 1
-    Your neighbor is a real estate agent and wants some help predicting housing prices for regions in the USA. It would be great if you could somehow create a model
-    for her that allows her to put in a few features of a house and returns back an estimate of what the house would sell for.
-    She has asked you if you could help her out with your new data science skills. You say yes, and decide that Linear Regression might be a good path to solve this problem!
-    '''
+if __name__ == '__main__':        
     df = pd.read_csv('data/USA_Housing.csv')
-    model = LinearRegression(df)
-    
-    x,y = model.prepare_data(df,'Address','Price')  
-    x_train,y_train,x_test,y_test = model.train_test_split(x,y,0.7)
-    model.fit(x_train,y_train)
-    results = model.predict(x_test,y_test)
-  
-    '''
-    QUESTION 2
-    Congratulations! You just got some contract work with an Ecommerce company based in New York City that sells clothing online but 
-    they also have in-store style and clothing advice sessions. Customers come in to the store, have sessions/meetings with a personal stylist, 
-    then they can go home and order either on a mobile app or website for the clothes they want.
-
-    The company is trying to decide whether to focus their efforts on their mobile app experience or their website. They've hired you 
-    on contract to help them figure it out! Let's get started!
-
-    Do you think the company should focus more on their mobile app or on their website?
-
-    This is tricky, there are two ways to think about this: Develop the Website to catch up to the performance of the mobile app, 
-    or develop the app more since that is what is working better. This sort of answer really depends on the other factors going on
-    at the company, you would probably want to explore the relationship between Length of Membership and the App or the Website before coming to a conclusion!
-    '''
-    df = pd.read_csv('data/Ecommerce Customers.csv')
-    model = LinearRegression(df)
-    
+    model = MultipleLinearRegression()
     #model.explore_data(df)
-    x,y = model.prepare_data(df,['Email','Address','Avatar'],'Yearly Amount Spent')
-    x_train,y_train,x_test,y_test = model.train_test_split(x,y,0.7)
+
+    # clean data
+    df.drop('Address',axis=1,inplace=True)
+
+    # split data
+    cols = list(df.columns)
+    x = df[cols[:-1]]
+    y = df[cols[-1]]
+    x_train,y_train,x_test,y_test = model.train_test_split(x,y,.6)
+    
+    # fit training data to model
     model.fit(x_train,y_train)
-    results = model.predict(x_test,y_test)
+
+    # predict
+    preds = model.predict(x_test)
+    x_test['Actual'] = y_test
+    x_test['Prediction'] = preds
+    print(x_test)
