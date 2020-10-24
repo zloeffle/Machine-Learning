cff8542d8678cdad11f7804df89d6c1818779f06
zloeffle <zloeffler22@gmail.com>
2020-10-23 19:46:52 -0500

    finished logistic regression

--- Regression/LogisticRegression.py
+++ Regression/LogisticRegression.py
@@ -9,7 +9,7 @@ class LogisticRegression:
         self.n_iter = num_iter # number of iterations for the gradient descent 
         self.y = y[:,np.newaxis] # increase dimension of target val array to imitate a col vector
         self.n_samples = len(self.y) # num of rows
-        self.x = np.hstack((np.ones((self.n_samples,1)),x))
+        self.x = np.hstack((np.ones((self.n_samples,1)),(x-np.mean(x,0))/np.std(x,0)))
         self.n_features = np.size(self.x,1) # num of features
         self.params = np.zeros((self.n_features,1)) # initialize params as 0
 
@@ -32,12 +32,10 @@ class LogisticRegression:
     '''
     Predict the target values using new data
     '''
-    def predict(self,x_test):
-        #n_samples = np.size(x_test,0)
-        #x_test = np.hstack((np.ones((n_samples,1)),x_test))
-        print(x_test)
-        print(self.params)
-        return self.sigmoid(x_test @ self.params)
+    def predict(self,x):
+        n_samples = np.size(x,0)
+        x = np.hstack((np.ones((n_samples,1)),(x-np.mean(x,0))/np.std(x,0)))
+        return np.round(self.sigmoid(x @ self.params))
     
     
 if __name__ == '__main__':        
@@ -52,12 +50,13 @@ if __name__ == '__main__':
     y_train = df[df.columns[-1]].iloc[:600].to_numpy()
     x_test = df[df.columns[:-1]].iloc[600:].to_numpy()
     y_test = df[df.columns[-1]].iloc[600:].to_numpy()
-    
-    print(x_train)
-    print(y_train)
 
     model = LogisticRegression(x_train,y_train)
     model.gradient_descent()
-    #pred = model.predict(x_test)
-    #print(pred)
+    pred1 = model.predict(x_train)
+    pred2 = model.predict(x_test)
+    train = df.iloc[:600]
+    train['Pred'] = pred1
+    test = df.iloc[600:]
+    test['Pred'] = pred2
     
